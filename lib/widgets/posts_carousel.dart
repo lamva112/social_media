157d235aafd67ae09da7f22bf54c5cc7ed0e3744
lambui <lambui@lecle.co.kr>
2023-07-31 20:16:08 +0700

    add pageview

--- lib/widgets/posts_carousel.dart
+++ lib/widgets/posts_carousel.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../data/data.dart';
 import '../models/post_model.dart';
 
 class PostsCarousel extends StatelessWidget {
@@ -10,90 +11,108 @@ class PostsCarousel extends StatelessWidget {
   const PostsCarousel({super.key, this.pageController, this.title = '', this.post = const []});
 
   Widget _buildPost(context, index) {
-    return Stack(
-      children: [
-        Container(
-          margin: const EdgeInsets.all(10),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(15.0),
-            boxShadow: const [BoxShadow(color: Colors.black26, offset: Offset(0, 2), blurRadius: 6.0)],
+    Post post = posts[index];
+    double value = (index == 1) ? 0.75 : 1;
+
+    return AnimatedBuilder(
+      animation: pageController!,
+      builder: (BuildContext context,Widget? widget){
+        if(pageController!.position.haveDimensions){
+          value = ((pageController!.page ?? 0) - index);
+          value = (1 - (value.abs() * 0.25)).clamp(0.0, 1.0);
+        }
+        return Center(
+          child: SizedBox(
+            height: Curves.easeInOut.transform(value) * 400.0,
+            child: widget,
           ),
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(15.0),
-            child: Image(
-              height: 400.0,
-              width: 300.0,
-              image: AssetImage(post[0].imageUrl),
-              fit: BoxFit.cover,
+        );
+      },
+      child: Stack(
+        children: [
+          Container(
+            margin: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15.0),
+              boxShadow: const [BoxShadow(color: Colors.black26, offset: Offset(0, 2), blurRadius: 6.0)],
             ),
-          ),
-        ),
-        Positioned(
-          left: 10.0,
-          right: 10.0,
-          bottom: 10.0,
-          child: Container(
-            padding: const EdgeInsets.all(12.0),
-            height: 110,
-            decoration: const BoxDecoration(
-              color: Colors.white54,
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(15.0),
-                bottomRight: Radius.circular(15.0),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(15.0),
+              child: Image(
+                height: 400.0,
+                width: 300.0,
+                image: AssetImage(post.imageUrl),
+                fit: BoxFit.cover,
               ),
             ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  post[0].title,
-                  style: const TextStyle(
-                    fontSize: 24.0,
-                    fontWeight: FontWeight.bold,
+          ),
+          Positioned(
+            left: 10.0,
+            right: 10.0,
+            bottom: 10.0,
+            child: Container(
+              padding: const EdgeInsets.all(12.0),
+              height: 110,
+              decoration: const BoxDecoration(
+                color: Colors.white54,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(15.0),
+                  bottomRight: Radius.circular(15.0),
+                ),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    post.title,
+                    style: const TextStyle(
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    overflow: TextOverflow.ellipsis,
                   ),
-                  overflow: TextOverflow.ellipsis,
-                ),
-                const SizedBox(
-                  height: 6.0,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Row(
-                      children: [
-                        const Icon(
-                          Icons.favorite,
-                          color: Colors.red,
-                        ),
-                        const SizedBox(
-                          width: 6.0,
-                        ),
-                        Text(
-                          post[0].likes.toString(),
-                          style: const TextStyle(fontSize: 18),
-                        )
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        Icon(
-                          Icons.comment,
-                          color: Theme.of(context).primaryColor,
-                        ),
-                        const SizedBox(
-                          width: 6.0,
-                        ),
-                        Text(post[0].comments.toString()),
-                      ],
-                    ),
-                  ],
-                )
-              ],
+                  const SizedBox(
+                    height: 6.0,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          const Icon(
+                            Icons.favorite,
+                            color: Colors.red,
+                          ),
+                          const SizedBox(
+                            width: 6.0,
+                          ),
+                          Text(
+                            post.likes.toString(),
+                            style: const TextStyle(fontSize: 18),
+                          )
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.comment,
+                            color: Theme.of(context).primaryColor,
+                          ),
+                          const SizedBox(
+                            width: 6.0,
+                          ),
+                          Text(post.comments.toString()),
+                        ],
+                      ),
+                    ],
+                  )
+                ],
+              ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 
@@ -112,6 +131,7 @@ class PostsCarousel extends StatelessWidget {
         SizedBox(
           height: 400,
           child: PageView.builder(
+            controller: pageController,
               itemCount: post.length,
               itemBuilder: (_, index) {
                 return _buildPost(context, index);
